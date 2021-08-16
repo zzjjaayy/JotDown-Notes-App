a03085c6d31aa4726c7aff4fd89ea5ab3b22abe1
zzjjaayy <jayprimerathod@gmail.com>
2021-08-16 18:40:14 +0530

    Modified add fragment layout and set up viewBinding & action w/ action bar

--- app/src/main/java/com/jay/todoapp/MainActivity.kt
+++ app/src/main/java/com/jay/todoapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.jay.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // managing action bar with actions in the nav host fragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+    // The up action in the action bar
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/jay/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/jay/todoapp/fragments/list/ListFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.jay.todoapp.R
+import com.jay.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
-     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_list, container, false)
-        }
+    private var _binding : FragmentListBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+           inflater: LayoutInflater, container: ViewGroup?,
+           savedInstanceState: Bundle?
+       ): View? {
+            // Inflate the layout for this fragment
+            val fragmentBinding = FragmentListBinding.inflate(inflater, container, false)
+            _binding = fragmentBinding
+            return fragmentBinding.root
+       }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+    }
 }
