8e1dfbc05de774b28588ac4a512df4ea88cdfe6f
zzjjaayy <jayprimerathod@gmail.com>
2021-08-17 17:29:00 +0530

    Created an Entity data class and a enum class for Priority

---
+++ app/src/main/java/com/jay/todoapp/data/Priority.kt
@@ -0,0 +1,6 @@
+package com.jay.todoapp.data
+
+// This is an enum classes which holds constants and this will be used in one of the columns of the DB.
+enum class Priority {
+    HIGH, MEDIUM, LOW
+}
