f970385da9fa78ce3536a06d2f348e59cd5e7d42
zzjjaayy <jayprimerathod@gmail.com>
2021-08-20 14:13:07 +0530

    Created a RecyclerViewAdapter

---
+++ app/src/main/java/com/jay/todoapp/fragments/list/ToDoAdapter.kt
@@ -0,0 +1,41 @@
+package com.jay.todoapp.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.jay.todoapp.R
+import com.jay.todoapp.data.model.Priority
+import com.jay.todoapp.data.model.ToDoData
+
+class ToDoAdapter(private val dataSet: List<ToDoData>) : RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>(){
+
+    class ToDoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val toDoTitle: TextView = view.findViewById(R.id.to_do_title)
+        val toDoDesc: TextView = view.findViewById(R.id.to_do_description)
+        val toDoPriority: CardView = view.findViewById(R.id.priority_indicator)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.to_do_layout, parent, false)
+        return ToDoViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        val item = dataSet[position]
+        holder.toDoTitle.text = item.title
+        holder.toDoDesc.text = item.description
+        when(item.priority) {
+            Priority.HIGH -> holder.toDoPriority.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+            Priority.MEDIUM -> holder.toDoPriority.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.yellow))
+            Priority.LOW -> holder.toDoPriority.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+        }
+    }
+
+    override fun getItemCount(): Int = dataSet.size
+
+    fun setData() {notifyDataSetChanged()}
+}
