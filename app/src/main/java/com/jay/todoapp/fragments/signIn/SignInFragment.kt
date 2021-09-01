2d043c336b99c93ebc092921398a0b4a64ad913d
zzjjaayy <jayprimerathod@gmail.com>
2021-09-01 15:47:14 +0530

    Added Firebase authentication with a new signIn Fragment

---
+++ app/src/main/java/com/jay/todoapp/fragments/signIn/SignInFragment.kt
@@ -0,0 +1,106 @@
+package com.jay.todoapp.fragments.signIn
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.jay.todoapp.R
+import com.jay.todoapp.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+
+    private var _binding : FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
+
+    companion object {
+        // This can be any number you want
+        private const val RC_SIGN_IN = 120
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        // auth instance
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.signInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if(task.isSuccessful) {
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    val account = task.getResult(ApiException::class.java)!!
+                    Log.d("jayischecking", "firebaseAuthWithGoogle:" + account.id)
+                    firebaseAuthWithGoogle(account.idToken!!)
+                } catch (e: ApiException) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("jayischecking", "Google sign in failed", e)
+                }
+            } else {
+                Log.w("jayischecking", exception.toString())
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("jayischecking", "signInWithCredential:success")
+                    findNavController().navigate(R.id.action_signInFragment_to_listFragment)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("jayischecking", "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+}
--- app/src/main/java/com/jay/todoapp/onboarding/splashScreen/SplashFragment.kt
+++ app/src/main/java/com/jay/todoapp/onboarding/splashScreen/SplashFragment.kt
@@ -3,24 +3,36 @@ package com.jay.todoapp.onboarding.splashScreen
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.jay.todoapp.R
 
 
 class SplashFragment : Fragment() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
-        Handler().postDelayed({
+
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+
+        Handler(Looper.getMainLooper()).postDelayed({
             if(startOnBoarding()) {
-                findNavController().navigate(R.id.action_splashFragment_to_listFragment)
+                if(currentUser == null) {
+                    findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
+
+                } else findNavController().navigate(R.id.action_splashFragment_to_listFragment)
             } else findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
           }, 1000)
         // Inflate the layout for this fragment
